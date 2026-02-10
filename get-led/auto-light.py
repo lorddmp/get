9fc8f967f09a2b6a2eee6a1c78989f85b8ba2dc2
lorddmp <dpavlov-07@mail.ru>
2026-02-10 16:39:53 +0300

    Task is done

--- get-led/auto-light.py
+++ get-led/auto-light.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 
 led = 26
 GPIO.setup(led, GPIO.OUT)
 state = 0
-period = 1.0
+photo_trans = 6
+GPIO.setup(photo_trans, GPIO.IN)
 
 while True:
+    state = GPIO.input(photo_trans)
     state = not state
     GPIO.output(led, state)
-    time.sleep(period)
---
+++ get-led/bin-counter.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+GPIO.setup(leds, GPIO.OUT)
+
+up_button = 9
+GPIO.setup(up_button, GPIO.IN)
+low_button = 10
+GPIO.setup(low_button, GPIO.IN)
+
+num = 0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+sleep_time = 0.2
+
+while True:
+    if GPIO.input(up_button):
+        num = num + 1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    elif GPIO.input(low_button):
+        num = num - 1
+        print(num, dec2bin(num))
+        time.sleep(sleep_time)
+    if num < 0 or num > 256:
+        num = 0
+    GPIO.output(leds, dec2bin(num))
